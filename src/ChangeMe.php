e30b2c74d8e2d22b8c7a8e0e32e65a950aa9fe9d
Yehor Chernyshov <yehor.chernyshov+github@gmail.com>
2024-05-27 11:42:55 +0200

    implement pass leng and upper case checks

--- src/ChangeMe.php
+++ src/ChangeMe.php
@@ -6,8 +6,16 @@ namespace Kata;
 
 final class ChangeMe
 {
-    public function changeMe(): bool
+    public function isPassValid(string $pass): bool
     {
+        if(strlen($pass) <= 8){
+            return false;
+        }
+
+        if (strtolower($pass) === $pass){
+            return false;
+        }
+
         return true;
     }
 }
--- tests/ChangeMeTest.php
+++ tests/ChangeMeTest.php
@@ -9,10 +9,37 @@ use PHPUnit\Framework\TestCase;
 
 final class ChangeMeTest extends TestCase
 {
-    public function test_change_me(): void
+    static public function invalidPassordDataProvider(): array
+    {
+        return [
+            'empty string' => ['pass' => '',  'expected' => false],
+            '8 chars' => ['pass' => '01234567',  'expected' => false],
+            '<8 chars' => ['pass' => '0123456',  'expected' => false],
+            'without UpperCase' => ['pass' => '012345678i',  'expected' => false],
+        ];
+    }
+
+
+    static public function validPassordDataProvider(): array
+    {
+        return [
+            '8+ chars | with UpperCase' => ['pass' => '01234567I',  'expected' => true],
+        ];
+    }
+
+
+    /** 
+     *  @dataProvider validPassordDataProvider()
+     *  @dataProvider invalidPassordDataProvider()
+     */
+    public function test_change_me(string $pass, bool $expected): void
     {
         $changeMe = new ChangeMe();
 
-        self::assertTrue($changeMe->changeMe());
+        self::assertSame($expected, $changeMe->isPassValid($pass));
     }
+
+
+
+
 }
